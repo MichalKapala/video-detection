9fbd0fac25d51dbf93b324976fe9f29bb3dbf1a7
MichalKapala <michal.kapala99@gmail.com>
2023-06-03 14:45:23 +0200

    Updated test image and added detection drawer

--- Tests/TestHumanDetector.py
+++ Tests/TestHumanDetector.py
@@ -7,6 +7,7 @@ class TestHumanDetector(unittest.TestCase):
 
     def test_detection_presence(self):
         img = cv2.imread("resources/test_img_1.jpg")
+        img = cv2.resize(img, (800, 800))
         detector = HogHumanDetector()
         detections = detector.get_detections(img)
         self.assertGreater(len(detections), 0)
