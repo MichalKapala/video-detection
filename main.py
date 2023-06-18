5d4da71f29ddc28a8ba860574208950673f9dbf3
MichalKapala <michal.kapala99@gmail.com>
2023-06-18 12:27:02 +0200

    Added dark theme

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from PyQt6.QtWidgets import QApplication
 from View.MainView import VideoPlayer
 
+import qdarktheme
+
 import sys
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    qdarktheme.setup_theme()
     main_window = VideoPlayer()
     main_window.resize(800, 600)
     main_window.show()
